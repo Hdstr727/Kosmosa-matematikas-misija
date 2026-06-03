edead83fb9bbc6eeb533fd2efbb1fb301e2c1e21
Hdstr727 <helldestroerr05@gmail.com>
2026-06-03 23:28:21 +0300

    Removed unused file

--- entities/__init__.py
+++
@@ -1,5 +0,0 @@
-# Entities pakotne / Entities package
-from entities.base_entity import BaseEntity
-from entities.ship import Ship
-from entities.asteroid import Asteroid
-from entities.pickup import Pickup, HealthPickup, FuelPickup
